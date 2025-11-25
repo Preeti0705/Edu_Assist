8869f0df78aca922d9c60900386f7de7e675fbcc
Preeti0705 <154693342+Preeti0705@users.noreply.github.com>
2025-11-25 12:40:53 +0530

    Update build directory configuration in build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,7 +3,7 @@ allprojects {
         google()
         mavenCentral()
     }
-}
+}   
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
